c0b3bab092376d677e83984aa0d1e9c2c1d9ddab
GustavoVito <gustavocarmovito@gmail.com>
2024-03-28 18:50:20 -0300

    adding header itens

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
+import logo from './assets/logo-portfolio.svg'
+import github from './assets/github.svg'
+import linkedin from './assets/linkedin.svg'
+
 export function App() {
   return  (
-  <h1 className="text-3xl font-bold underline">
-  Hello world!
-  </h1>
+    <div className='mx-auto max-w-6xl my-3'>
+      <img src={logo} alt="logo do projeto" className='w-60'/>
+      <a href="">Projetos</a>
+      <a href="">Sobre mim</a>
+      <a href="https://github.com/GustavoVito"><img src={github} alt="link para o github" className='w-5'/>  </a>
+      <a href="https://www.linkedin.com/in/gustavo-do-carmo-vito-7a41ab217/"><img src={linkedin} alt="link para o linkedin" className='w-5'/>  </a>
+
+    </div>
 )
 }
 
