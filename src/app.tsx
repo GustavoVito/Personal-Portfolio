c47bbfe8ca0b8b0796a313132c3c1f252668cd39
GustavoVito <gustavocarmovito@gmail.com>
2024-03-29 16:04:47 -0300

    header complete

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,30 @@
 import logo from './assets/logo-portfolio.svg'
 import github from './assets/github.svg'
 import linkedin from './assets/linkedin.svg'
+import dark2 from './assets/dark2.svg'
 
 export function App() {
   return  (
-    <div className='mx-auto max-w-6xl space-x-48 my-3 flex items-center'>
+    <div className='mx-auto max-w-6xl '>
+    <div className=' space-x-48 mt-3 flex items-center'>
       <img src={logo} alt="logo do projeto" className='w-60'/>
 
-      <div className='space-x-10'>
+      <div className='space-x-16 mr-12 text-lg'>
         <a href="">Projetos</a>
         <a href="">Sobre mim</a>
       </div>
 
-      <div className='space-x-2 flex'>
-        <a href="https://github.com/GustavoVito"><img src={github} alt="link para o github" className='w-5'/>  </a>
-        <a href="https://www.linkedin.com/in/gustavo-do-carmo-vito-7a41ab217/"><img src={linkedin} alt="link para o linkedin" className='w-5'/>  </a>
+      <div className='space-x-2 flex '>
+        <a href="https://github.com/GustavoVito"><img src={github} alt="link para o github" className='w-7' />  </a>
+        <a href="https://www.linkedin.com/in/gustavo-do-carmo-vito-7a41ab217/"><img src={linkedin} alt="link para o linkedin" className='w-7'/>  </a>
       </div>
 
-      <div className='h-1 bg-red-700'/>
+      <img src={dark2} alt="" className='w-7'/>
 
+      
+
+    </div>
+    <div className='h-0.5 bg-red-700 rounded-full'/>
     </div>
     )
 }
